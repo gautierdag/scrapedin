b15acb342fa220760d8dee09325d521fd824290b
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2020-06-04 16:17:49 +0100

    feat: use chromium instead of puppeteer

--- src/scrapedin.js
+++ src/scrapedin.js
@@ -1,31 +1,62 @@
-const puppeteer = require('puppeteer')
-const login = require('./login')
-const profile = require('./profile/profile')
-const logger = require('./logger')(__filename)
+const chromium = require("chrome-aws-lambda");
+const login = require("./login");
+const profile = require("./profile/profile");
+const logger = require("./logger")(__filename);
 
-module.exports = async ({ cookies, email, password, isHeadless, hasToLog, hasToGetContactInfo, puppeteerArgs, puppeteerAuthenticate } = { isHeadless: true, hasToLog: false }) => {
-  if (!hasToLog) {
-    logger.stopLogging()
+module.exports = async (
+  {
+    cookies,
+    email,
+    password,
+    isHeadless,
+    hasToLog,
+    hasToGetContactInfo,
+    puppeteerArgs,
+    puppeteerAuthenticate
+  } = {
+    isHeadless: true,
+    hasToLog: false
   }
-  logger.info('initializing')
+) => {
+  if (!hasToLog) {
+    logger.stopLogging();
+  }
+  logger.info("initializing");
 
-  const args = Object.assign({ headless: isHeadless, args: ['--no-sandbox'] }, puppeteerArgs)
-  const browser = await puppeteer.launch(args)
+  const args = {
+    args: chromium.args.concat(["--disable-features=AudioServiceOutOfProcess"]),
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true
+  };
+
+  const browser = await chromium.puppeteer.launch(args);
 
   if (cookies) {
-    logger.info('using cookies, login will be bypassed')
+    logger.info("using cookies, login will be bypassed");
   } else if (email && password) {
-    logger.info('email and password was provided, we\'re going to login...')
+    logger.info("email and password was provided, we're going to login...");
 
     try {
-      await login(browser, email, password, logger)
+      await login(browser, email, password, logger);
     } catch (e) {
-      await browser.close()
-      throw e
+      await browser.close();
+      throw e;
     }
   } else {
-    logger.warn('email/password and cookies wasn\'t provided, only public data will be collected')
+    logger.warn(
+      "email/password and cookies wasn't provided, only public data will be collected"
+    );
   }
 
-  return (url, waitMs) => profile(browser, cookies, url, waitMs, hasToGetContactInfo, puppeteerAuthenticate)
-}
+  return (url, waitMs) =>
+    profile(
+      browser,
+      cookies,
+      url,
+      waitMs,
+      hasToGetContactInfo,
+      puppeteerAuthenticate
+    );
+};

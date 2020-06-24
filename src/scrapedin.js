5b18c354bc8caf1fe1251851a62b7a2517952a51
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2020-06-24 11:14:01 +0100

    build: style change

--- src/scrapedin.js
+++ src/scrapedin.js
@@ -3,20 +3,35 @@ const login = require('./login')
 const profile = require('./profile/profile')
 const logger = require('./logger')(__filename)
 
-module.exports = async ({ cookies, email, password, isHeadless, hasToLog, hasToGetContactInfo, puppeteerArgs, puppeteerAuthenticate, endpoint } = { isHeadless: true, hasToLog: false }) => {
+module.exports = async ({
+  cookies,
+  email,
+  password,
+  isHeadless,
+  hasToLog,
+  hasToGetContactInfo,
+  puppeteerArgs,
+  puppeteerAuthenticate,
+  endpoint
+} = {
+  isHeadless: true,
+  hasToLog: false
+}) => {
   if (!hasToLog) {
     logger.stopLogging()
   }
   logger.info('initializing')
 
   let browser;
-  if(endpoint){
+  if (endpoint) {
     browser = await chromium.puppeteer.connect({
       browserWSEndpoint: endpoint,
     });
-  }else{
-    const args = Object.assign({ headless: isHeadless, args: ['--no-sandbox'] }, puppeteerArgs)
-    browser = await puppeteer.launch(args)
+  } else {
+    const args = Object.assign({
+      headless: isHeadless,
+      args: ['--no-sandbox']
+    }, puppeteerArgs)
     browser = await chromium.puppeteer.launch(args)
   }
 
@@ -28,7 +43,7 @@ module.exports = async ({ cookies, email, password, isHeadless, hasToLog, hasToG
     try {
       await login(browser, email, password, logger)
     } catch (e) {
-      if(!endpoint){
+      if (!endpoint) {
         await browser.close()
       }
       throw e
